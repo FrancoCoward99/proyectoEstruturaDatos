804c91f824efe6934f9419b659a0e9c9cc4c869f
FrancoCoward99 <franco99cr@gmail.com>
2024-06-30 21:39:24 -0600

    Agrego en la clase nodo los atributos del cliente y ademas en clase proyecto que se suba la info en el txt llamdo prod

--- Proyecto/src/proyecto/Nodo.java
+++ Proyecto/src/proyecto/Nodo.java
@@ -1,15 +1,117 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyecto;
+import java.time.LocalDateTime;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Admin
  */
 public class Nodo {
+    //Como se indica en el modulo 1.1 se debe cotemplar la informacion del cliente en la clase nodo
+    private String nombre;
+    private String id;
+    private int edad;
+    private LocalDateTime horaCreacion;
+    private LocalDateTime horaAtencion;
+    private String tramite;
+    private char tipo; // nivel de prioridad A,B,C 
+    private int prioridad; // Para manejar la prioridad en la cola
+
+    public Nodo(String nombre, String id, int edad,  String tramite, char tipo) {
+        this.nombre = nombre;
+        this.id = id;
+        this.edad = edad;
+        this.horaCreacion = horaCreacion;
+        this.horaAtencion = horaAtencion;
+        this.tramite = tramite;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public LocalDateTime getHoraCreacion() {
+        return horaCreacion;
+    }
+
+    public void setHoraCreacion(LocalDateTime horaCreacion) {
+        this.horaCreacion = horaCreacion;
+    }
+
+    public LocalDateTime getHoraAtencion() {
+        return horaAtencion;
+    }
+
+    public void setHoraAtencion(LocalDateTime horaAtencion) {
+        this.horaAtencion = horaAtencion;
+    }
+
+    public String getTramite() {
+        return tramite;
+    }
+
+    public void setTramite(String tramite) {
+        this.tramite = tramite;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
     
+    public void setPrioridad(){
+        switch(tipo){
+            case 'A':
+                this.prioridad =1;
+                break;
+            case 'B':
+                this.prioridad = 2;
+                 break;
+            case 'C':
+                this.prioridad = 3;
+                break;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente " +"\n"+
+                "Nombre: " + nombre +"\n"+ 
+                "Id: " + id +"\n"+ 
+                "Edad: " + edad +"\n"+ 
+                "HoraCreacion=" + horaCreacion +"\n"+ "Hora de atencion" + horaAtencion +"\n"+ 
+                "Tramite: " + tramite +"\n"+ 
+                "Tipo: " + tipo +"\n"+ 
+                "Prioridad: " + prioridad;
+    }
     
     
 }
--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -4,7 +4,12 @@
  */
 package proyecto;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -16,6 +21,12 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        List<Nodo> clientes = new ArrayList<>();
+        clientes.add(new Nodo("Juan Perez", "123", 30, "Depósitos", 'P'));
+        clientes.add(new Nodo("Maria Lopez", "124", 25, "Retiros", 'A'));
+        clientes.add(new Nodo("Carlos Sanchez", "125", 40, "Cambio de Divisas", 'B'));
+         
+       guardarCliente_archivo(clientes);
         Banco configuracion;
         File txt = new File("prod.txt");
         if (txt.exists()) {
@@ -28,6 +39,17 @@ public class Proyecto {
         
     }
     
-    
+        public static void guardarCliente_archivo(List<Nodo> lista){
+        try{
+            BufferedWriter ingreso = new BufferedWriter(new FileWriter("prod.txt", true));
+            for(Nodo ls : lista){
+                ingreso.write(ls.getNombre()+"\t"+ls.getId()+"\t"+String.valueOf(ls.getEdad())+"\t"+ls.getTramite()+"\t"+ls.getTipo());
+                ingreso.newLine();
+            }
+            ingreso.close();
+        }catch(IOException e){
+             System.out.println("Error al escribir en el archivo " + e.getMessage());
+        }
+    }
     
 }
