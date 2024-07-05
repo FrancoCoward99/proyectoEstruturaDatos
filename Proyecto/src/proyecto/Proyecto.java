3fdab438c190a49274b700bcef8ed2378341a903
FrancoCoward99 <franco99cr@gmail.com>
2024-07-05 08:59:33 -0600

    Agrego el formulario de registro de clientes y que se agregue en el txt de prod con btn de guardar y btn cancelar

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
  *
  * @author Admin
@@ -21,12 +22,14 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        //Lista de ejemplo
         List<Nodo> clientes = new ArrayList<>();
         clientes.add(new Nodo("Juan Perez", "123", 30, "Depósitos", 'P'));
         clientes.add(new Nodo("Maria Lopez", "124", 25, "Retiros", 'A'));
         clientes.add(new Nodo("Carlos Sanchez", "125", 40, "Cambio de Divisas", 'B'));
-         
-       guardarCliente_archivo(clientes);
+
+        guardarCliente_archivo(clientes);
         Banco configuracion;
         File txt = new File("prod.txt");
         if (txt.exists()) {
@@ -34,22 +37,22 @@ public class Proyecto {
         } else {
             configuracion = Banco.obtenerConfiguracion();
         }
-        
+
         //File txt = new File("prod.txt");
-        
+         
     }
-    
-        public static void guardarCliente_archivo(List<Nodo> lista){
-        try{
+
+    public static void guardarCliente_archivo(List<Nodo> lista) {
+        try {
             BufferedWriter ingreso = new BufferedWriter(new FileWriter("prod.txt", true));
-            for(Nodo ls : lista){
-                ingreso.write(ls.getNombre()+"\t"+ls.getId()+"\t"+String.valueOf(ls.getEdad())+"\t"+ls.getTramite()+"\t"+ls.getTipo());
+            for (Nodo ls : lista) {
+                ingreso.write(ls.getNombre() + "\t" + ls.getId() + "\t" + String.valueOf(ls.getEdad()) + "\t" + ls.getTramite() + "\t" + ls.getTipo());
                 ingreso.newLine();
             }
             ingreso.close();
-        }catch(IOException e){
-             System.out.println("Error al escribir en el archivo " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo " + e.getMessage());
         }
     }
-    
+
 }
