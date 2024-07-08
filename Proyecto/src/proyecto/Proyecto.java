09a237c6c58d8faf1417892b670cf9f0b661fbed
saek27 <162070809+saek27@users.noreply.github.com>
2024-07-07 18:54:36 -0600

    Correcion de error en main
    Correcion de error en main

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -26,47 +26,40 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
         InfoSesion sesion = new InfoSesion("admin", "admin");
-        
-       
+
         //Mostra UI
         Login loginUi = new Login();
         MenuPrincipal menuPrincipalUI = new MenuPrincipal();
         FormCliente formClientesUI = new FormCliente();
+        loginUi.setVisible(true);
+        iniciarAplicacion();
 
         //Lista de ejemplo
-        List<Nodo> clientes = new ArrayList<>();
+        //List<Nodo> clientes = new ArrayList<>();
         //clientes.add(new Nodo("Juan Perez", "123", 30, "Depósitos", 'P'));
         //clientes.add(new Nodo("Maria Lopez", "124", 25, "Retiros", 'A'));
-       // clientes.add(new Nodo("Carlos Sanchez", "125", 40, "Cambio de Divisas", 'B'));
-
-        guardarCliente_archivo(clientes);
-        
+        // clientes.add(new Nodo("Carlos Sanchez", "125", 40, "Cambio de Divisas", 'B'));
+        //guardarCliente_archivo(clientes);
         //Cargar Conf TXT
-        Banco configuracion;
-        File txt = new File("prod.txt");
-        if (txt.exists()) {
-            configuracion = Banco.cargarConfiguracion();
-        } else {
-            configuracion = Banco.obtenerConfiguracion();
-        }
-
         //File txt = new File("prod.txt");
     }
 
-    public static void guardarCliente_archivo(List<Nodo> lista) {
-        try {
-            BufferedWriter ingreso = new BufferedWriter(new FileWriter("prod.txt", true));
-            for (Nodo ls : lista) {
-                ingreso.write(ls.getNombre() + "\t" + ls.getId() + "\t" + String.valueOf(ls.getEdad()) + "\t" + ls.getTramite() + "\t" + ls.getTipo());
-                ingreso.newLine();
-            }
-            ingreso.close();
-        } catch (IOException e) {
-            System.out.println("Error al escribir en el archivo " + e.getMessage());
-        }
-    }
+    private static void iniciarAplicacion() {
+        Banco configuracion;
+        File txt = new File("prod.txt");
 
+        // Verifica si el archivo existe y no está vacío
+        if (txt.exists() && txt.length() > 0) {
+            configuracion = Banco.cargarConfiguracion();
+            if (configuracion == null) {
+                // Si la configuración cargada es nula, solicita la configuración
+                configuracion = Banco.obtenerConfiguracion();
+            }
+        } else {
+            // Si el archivo no existe o está vacío, solicita la configuración
+            configuracion = Banco.obtenerConfiguracion();
+        }
+
+    }
 }
